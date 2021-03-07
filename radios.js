9b63a9515b6e10d3d0d6016d41289a1b89c558ca
DoNotSpamPls <hello@dnsp.dev>
2021-03-07 16:09:37 +0200

    Нови линкове

--- radios.js
+++ radios.js
@@ -1,7 +1,7 @@
 ﻿// Линкове към радиотата
 var radios = [
-    "http://lb-hls.cdn.bg/2032/fls/Horizont.stream/playlist.m3u8", // БНР Хоризонт
-    "http://lb-hls.cdn.bg/2032/fls/Shumen.stream/playlist.m3u8", // БНР Шумен
+    "https://lb-hls.cdn.bg/2032/fls/Horizont.stream/playlist.m3u8", // БНР Хоризонт
+    "https://lb-hls.cdn.bg/2032/fls/Shumen.stream/playlist.m3u8", // БНР Шумен
     "https://play.global.audio/radio1128", // Радио 1
     "https://play.global.audio/radio1rock128", // Радио 1 Рок
     "https://play.global.audio/bgradio128", // БГ Радио
@@ -33,10 +33,10 @@ $(document).ready(function(){
             case "Веселина": audioElement.setAttribute('src', ""); hls.loadSource(radios[6]); hls.attachMedia(audioElement); break;
             case "The Voice": audioElement.setAttribute('src', ""); hls.loadSource(radios[7]); hls.attachMedia(audioElement); break;
             case "Energy": audioElement.setAttribute('src', radios[8]); break;
-            case "N-Joy": audioElement.setAttribute('src', radios[9]); break;
+            //case "N-Joy": audioElement.setAttribute('src', radios[9]); break;
             case "City": audioElement.setAttribute('src', radios[10]); break;
-            case "Fresh!": audioElement.setAttribute('src', radios[11]); break;
-            case "Мая": audioElement.setAttribute('src', radios[12]); break;
+            //case "Fresh!": audioElement.setAttribute('src', radios[11]); break;
+            //case "Мая": audioElement.setAttribute('src', radios[12]); break;
             // Радиото не е в списъка (не би трябвало да се случва)
             default: alert("Грешка!");
         }
